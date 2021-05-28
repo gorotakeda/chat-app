a91516025f63ac5c1adbe4accdd10863b32ef611
gorotakeda <rugbytkg3@gmail.com>
2021-05-28 18:40:14 +0900

    消し方が分からん…

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  # ログインしていないユーザーをログインページの画面に促す
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
